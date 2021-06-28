709ca70a46255a86af3b5dd83b53e1d3c83c5fe9
amaliapena <amalia.leija.pena@gmail.com>
2021-06-28 14:04:22 -0700

    Encapsulation 3 codes complete so far

--- module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,45 @@ package com.techelevator;
 
 public class Elevator {
 
-}
+    private boolean openDoor;
+    private int numberOfFloors;
+    private int currentFloor;
+
+    public Elevator (int totalNumberOfFloors) {
+        this.numberOfFloors = totalNumberOfFloors;
+        currentFloor = 1;
+    }
+
+    public boolean isDoorOpen() {
+        return openDoor;
+
+    }
+     public int getNumberOfFloors() {
+        return numberOfFloors;
+     }
+
+     public int getCurrentFloor() {
+        return currentFloor;
+     }
+
+     public void openDoor() {
+        openDoor = true;
+     }
+
+     public void closeDoor() {
+        openDoor = false;
+     }
+
+     public void goUp (int desiredFloor) {
+         if (!openDoor && desiredFloor > currentFloor && desiredFloor <= numberOfFloors) {
+             currentFloor = desiredFloor;
+         }
+     }
+
+        public void goDown (int desiredFloor) {
+            if (!openDoor && desiredFloor >= 1 && desiredFloor < currentFloor) {
+                currentFloor = desiredFloor;
+            }
+         }
+     }
+
--- module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_Encapsulation/exercise-student/java/src/main/java/com/techelevator/FruitTree.java
@@ -2,4 +2,29 @@ package com.techelevator;
 
 public class FruitTree {
 
+    String typeOfFruit = "";
+
+    int piecesOfFruitLeft;
+
+    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+        this.typeOfFruit = typeOfFruit;
+        this.piecesOfFruitLeft = startingPiecesOfFruit;
+    }
+
+    public int getPiecesOfFruitLeft() {
+        return piecesOfFruitLeft;
+    }
+
+    public String getTypeOfFruit() {
+        return typeOfFruit;
+    }
+
+    public boolean pickFruit(int numberOfPiecesToRemove) {
+        if (piecesOfFruitLeft >= numberOfPiecesToRemove) {
+            piecesOfFruitLeft -= numberOfPiecesToRemove;
+            return true;
+        }
+        return false;
+    }
 }
+
