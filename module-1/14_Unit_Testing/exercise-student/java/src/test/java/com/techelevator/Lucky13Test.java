7722bedd08da673b96374db868351a182054f172
amaliapena <amalia.leija.pena@gmail.com>
2021-07-05 22:16:45 -0500

    Unit Testing Exercise

--- module-1/14_Unit_Testing/exercise-student/java/src/test/java/com/techelevator/Lucky13Test.java
+++ module-1/14_Unit_Testing/exercise-student/java/src/test/java/com/techelevator/Lucky13Test.java
@@ -1,4 +1,29 @@
 package com.techelevator;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
 public class Lucky13Test {
+    Lucky13 l13 = null;
+
+    @Before
+    public void setUp() {
+        l13 = new Lucky13();
+    }
+    @After
+    public void tearDown() {
+        l13 = null;
+    }
+    @Test
+    public void returnTrueIfArrayContainsNo1sAndNo3s() {
+        Assert.assertEquals("Incorrect, answer should be true.", l13.getLucky(new int[] {0, 2, 4}), true);
+        Assert.assertEquals("Incorrect, answer should be false.", l13.getLucky(new int[] {1, 2, 3}), false);
+        Assert.assertEquals("Incorrect, answer should be false.", l13.getLucky(new int[] {1, 2, 4}), false);
+
+
+    }
+
+
 }
